cd2ead602a93577d3b40118919bf96474df74656
todesking <discommunicative@gmail.com>
2017-10-22 20:58:03 +0900

    mathmd

---
+++ mathmd.d/dynoload.js
@@ -0,0 +1,4 @@
+var fileref=document.createElement('script')
+fileref.setAttribute("type","text/javascript")
+fileref.setAttribute("src", "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=TeX-AMS-MML_HTMLorMML")
+document.getElementsByTagName("head")[0].appendChild(fileref)
