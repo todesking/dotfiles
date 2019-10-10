9000755fe6b305b7619a579a2977eb0fbf5cb0a0
todesking <discommunicative@gmail.com>
2019-10-10 17:36:24 +0900

    mark_dup: prettier

--- nvim/rplugin/python3/denite/filter/converter/mark_dup.py
+++ nvim/rplugin/python3/denite/filter/converter/mark_dup.py
@@ -1,3 +1,4 @@
+import re
 from ..base import Base
 
 def decompose(a, b):
@@ -19,9 +20,11 @@ class Filter(Base):
         for candidate in context['candidates']:
             orig = candidate.get('mark_dup__orig') or candidate.get('abbr') or candidate['word']
             candidate['mark_dup__orig'] = orig
-            cur = orig.split('/')
+            cur = re.split(r'(\[.+\]|\/)', orig)
+            if cur and cur[0] == '':
+                cur = cur[1:]
             prefix, a, _ = decompose(cur, prev)
             if prefix != []:
-                candidate['abbr'] = '{{{' + '/'.join(prefix) + '}}}/' + '/'.join(a)
+                candidate['abbr'] = '{{{' + ''.join(prefix) + '}}}' + ''.join(a)
             prev = cur
         return context['candidates']
